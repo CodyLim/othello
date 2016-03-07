1413067f0784a397c83d83a9a860a96122270d83
CodyLim <cllim@caltech.edu>
2016-03-06 23:14:10 -0800

    Added value for player score in constructor.

--- player.cpp
+++ player.cpp
@@ -8,7 +8,8 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
+	int player_score = 0;
+	
     /* 
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
